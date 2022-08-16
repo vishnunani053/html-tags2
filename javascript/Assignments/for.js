0664ce956bb00e4088e67dcdafcca6dc0431420a
vishnunani053 <vishnulucky053@gmail.com>
2022-08-16 22:51:33 +0530

    jav

--- javascript/Assignments/for.js
+++ javascript/Assignments/for.js
@@ -23,3 +23,58 @@ for(let i=1;i<=10;i=i+2){
     console.log(i)
 }
 */
+
+
+
+
+//wap to print factors 32
+
+/*
+var num=32;
+ console.log("factors of 32 are")
+ for(let i=1;i<=num;i=i+1){
+    if(num%i==0)
+    console.log(i)
+ }
+*/
+
+
+
+
+/*
+
+//// program to generate fibonacci series up to a certain number
+
+// take input from the user
+const number = parseInt('Enter a positive number: ');
+let n1 = 0, n2 = 1,n3 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+console.log(n1); // print 0
+console.log(n2); // print 1
+console.log(n3);
+nextTerm = n1 + n2;
+nextTerm = n2 + n3;
+while (nextTerm <= number) {
+
+    // print the next term
+    console.log(nextTerm);
+
+    n1 = n2;
+    n2 = n3;
+    n3 = nextTerm;
+    nextTerm = n1 + n2;
+}  */
+
+
+
+
+
+
+
+
+
+
+
+
+
--- javascript/Assignments/while.js
+++ javascript/Assignments/while.js
@@ -8,6 +8,8 @@ while(i<=10){
 
 
 
+
+
 /*
 //wap to print 10 to 1 numbers
 var i=10;
@@ -17,6 +19,8 @@ while(i>=1){
     i=i-1;
 }   
 */
+
+
 
 
 
@@ -32,6 +36,9 @@ while(i>=1){
 
 
 
+
+
+
  // write a program first ten multiples of 4
  /* 
  var i=4;
@@ -39,6 +46,8 @@ while(i>=1){
     console.log(i);
     i=i+4
  }*/
+
+
 
 
 
@@ -54,6 +63,56 @@ while(i>=1){
 
 
 
- //wap for factors of 24
 
- var i=
+
+
+ /*
+ //wap for factors of 24
+ var num=24;
+ let i=1;
+ console.log("factors of 24 are");
+ while(i<=24){
+   i=i+1;
+   if(num%i==0){
+      console.log(i)
+   }
+ }
+*/
+
+
+
+
+
+
+
+
+//wap to print reverse of digits if numbers
+/*
+let rev = 0;
+let num = 123456;
+let lastDigit;
+
+while(num != 0){
+	lastDigit = num % 10;
+  rev = rev * 10 + lastDigit;
+  num = Math.floor(num/10);
+}
+
+console.log("Reverse number : "+rev);  
+*/
+
+
+
+
+
+
+
+//wap to read 5 numbers and print only even numbers
+
+var num=12345678;
+let i=1;
+while(i<=8){
+   if(num%2==0);
+   console.log(i)
+}
+
