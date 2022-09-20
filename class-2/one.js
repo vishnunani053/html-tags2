56d30cfcebabd217fbfbc91d9a939141420ff6e3
vishnunani053 <vishnulucky053@gmail.com>
2022-09-20 17:58:17 +0530

    its okk

--- class-2/one.js
+++ class-2/one.js
@@ -1,4 +1,4 @@
-class user{
+/*class user{
     
 constructor(){
     console.log("good employee");
@@ -10,3 +10,38 @@ new user()
 new user()
 new user()
 new user()
+
+
+class Parent {
+    assets = '100CR'
+    get_Good_Qualities() {
+        console.log("All Parent are Good")
+    }
+}
+class Child extends Parent {
+    get_Naughty_Qualities() {
+        console.log("All Children are naughty")
+    }
+}
+let c1 = new Child();
+console.log(c1.assets)
+c1.get_Good_Qualities()
+c1.get_Naughty_Qualities()*/
+
+
+
+class Parent{
+    assets="100cr"
+    get_good_qualities(){
+        console.log("all parents are good");
+    }
+}
+class Child extends Parent{
+    get_bad_qualities(){
+        console.log("all children are bad");
+    }
+}
+let c1=new Child();
+console.log(c1.assets);
+c1.get_good_qualities()
+c1.get_bad_qualities()
